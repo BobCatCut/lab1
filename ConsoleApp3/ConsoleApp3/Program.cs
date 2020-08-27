efc586bfdfd3db6c18e2ffb268b9901da7fee151
BobCatCut <67309750+BobCatCut@users.noreply.github.com>
2020-08-27 03:01:09 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class ThreadTest
+        {
+            static bool done; // Статичне поле, розділене потоками 
+            static void Main()
+            {
+                new Thread(Go).Start(); // новий потік викликає Go
+                Go(); // викликаємо Go в головному потоці
+            }
+            static void Go() // функція з умовою для виводу слова Done
+            {
+                if (!done) { done = true; Console.WriteLine("Done"); }
+            }
+        }
+    }
+}
