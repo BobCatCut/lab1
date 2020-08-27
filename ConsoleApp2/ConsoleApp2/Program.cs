a1e2bc942f5a841d39d483038f12d9b35a25cc2b
BobCatCut <67309750+BobCatCut@users.noreply.github.com>
2020-08-27 03:00:52 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class ThreadTest
+
+        {
+
+            bool done;
+            static void Main()
+            {
+                ThreadTest tt = new ThreadTest();
+                new Thread(tt.Go).Start();
+                tt.Go();
+            }
+            void Go()
+            {
+                if (!done) { done = true; Console.WriteLine("Done"); }
+            }
+        }
+    }
+}
