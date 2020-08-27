d3f6e601a6313810d3eb1588d810e627889042bc
BobCatCut <67309750+BobCatCut@users.noreply.github.com>
2020-08-27 03:00:28 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            new Thread(Go).Start();
+            Go();
+        }
+        static void Go()
+        {
+            for (int cycles = 0; cycles < 5; cycles++)
+                Console.Write('P');
+        }
+    }
+}
