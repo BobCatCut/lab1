950f458ee429fdded3db78dd1c012ff0d4e654d7
BobCatCut <67309750+BobCatCut@users.noreply.github.com>
2020-08-27 03:01:53 +0300

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        class ThreadSafe
+        {
+            static bool done; // Статичне поле, розділене потоками 
+            static object locker = new object();//статичний об’єкт locker для блокування потоку
+            static void Main()
+            {
+                new Thread(Go).Start(); // новий потік викликає Go
+                Go(); // викликаємо Go в головному потоці
+            }
+            static void Go()// функція з умовою для виводу слова Done
+            {
+                lock (locker)// оператор lock для блокування потоку
+                {
+                    if (!done)
+                    {
+                        Console.WriteLine("Done");
+                        done = true;
+                    }
+                }
+            }
+        }
+    }
+}
